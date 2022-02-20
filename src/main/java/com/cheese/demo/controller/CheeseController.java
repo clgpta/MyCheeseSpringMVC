39f7f17dd57a4861bd462a595610b4f50369e915
czou <clgpta@yahoo.com>
2022-02-20 10:09:52 -0500

    Initial commit

---
+++ src/main/java/com/cheese/demo/controller/CheeseController.java
@@ -0,0 +1,39 @@
+package com.cheese.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    static ArrayList<String> cheeses = new ArrayList<String>();
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheese");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add" , method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model)
+    {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add" , method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam(required=true ,name="cheeseName") String cheeseName)
+    {
+        cheeses.add(cheeseName);
+        //add a comment here.
+        return "redirect:";
+    }
+}
---
+++ src/test/java/com/cheese/demo/MyCheeseSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheese.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCheeseSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
